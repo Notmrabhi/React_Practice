2ad77501099865fd036376f3a1d47760581c5f87
Abhishek anand <110781004+Notmrabhi@users.noreply.github.com>
2023-12-26 00:27:33 +0530

    Update Forecast.jsx

--- React_Project7/src/components/Forecast.jsx
+++ React_Project7/src/components/Forecast.jsx
@@ -6,43 +6,42 @@ function Forecast() {
 
     const ref = useRef(null);
 
-    // const [deleteData, SetdeleteData] = useState({closee: false})
+    const [data, SetData] = useState([
+        {
+          id: 1,
+          desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe.",
+          filesize: ".9mb",
+          close: false,
+          tag: { isopen: true, tagtitle: "DownLoad Now", tagcolor: "green"},
+        },
+        {
+            id: 2,
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe .",
+            filesize: ".2mb",
+            close: false,
+            tag: { isopen: true, tagtitle: "Upload", tagcolor: "red"},
+        },
+        {
+            id: 3,
+          desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe.",
+          filesize: ".9mb",
+          close: false, 
+          tag: { isopen: true, tagtitle: "DownLoad Now", tagcolor: "green"},
+        },
+        {
+            id: 4,
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe.",
+            filesize: ".12mb",
+            close: true, 
+            tag: { isopen: false, tagtitle: "DownLoad Now", tagcolor: "green"},
+          },
+        
+      ]);
 
-    // const handleDelete = () =>{
-    //     SetdeleteData({closee: true})
-    // };
+    const handleDelete = (id) =>{
+        SetData(data.filter((data)=> data.id !== id));
+    };
   
-  const data = [
-    {
-      id: 1,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe.",
-      filesize: ".9mb",
-      close: false,
-      tag: { isopen: true, tagtitle: "DownLoad Now", tagcolor: "green"},
-    },
-    {
-        id: 2,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe .",
-        filesize: ".2mb",
-        close: false,
-        tag: { isopen: true, tagtitle: "Upload", tagcolor: "red"},
-    },
-    {
-        id: 3,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe.",
-      filesize: ".9mb",
-      close: false, 
-      tag: { isopen: true, tagtitle: "DownLoad Now", tagcolor: "green"},
-    },
-    {
-        id: 4,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe.",
-        filesize: ".12mb",
-        close: true, 
-        tag: { isopen: false, tagtitle: "DownLoad Now", tagcolor: "green"},
-      },
-    
-  ];
 
 
   return (
@@ -50,7 +49,9 @@ function Forecast() {
         <div ref={ref} className="fixed top-0 left-0 w-full h-full z-[3] flex gap-10 flex-wrap p-5">
            {data.map((item, index)=> (
             <Card 
+            key={item.id}
             data={item} 
+            onDelete={handleDelete}
             referance={ref}
             />
            ))}
